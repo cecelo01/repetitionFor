100e973c8121e78860dc827058c3e3666b2fa43b
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-05-24 09:24:21 -0300

    up

---
+++ src/br/repetitionfor/RepetitionFor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.repetitionfor;
+
+/**
+ *
+ * @author Marce
+ */
+public class RepetitionFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int cc=31 ; cc < 33 ; cc +=2){
+            System.out.println("Cambalhota"+cc);
+        }
+    }
+    
+}
